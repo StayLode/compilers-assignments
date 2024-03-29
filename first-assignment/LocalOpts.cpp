84c29b6c6d81fd19de5c754478c7b23ffdb6a708
StayLode <luca.lode02@gmail.com>
2024-03-29 16:55:05 +0100

    Multi-Instruction optimization

--- first-assignment/LocalOpts.cpp
+++ first-assignment/LocalOpts.cpp
@@ -22,15 +22,15 @@ bool isCloseToPow2(ConstantInt*);
 bool isPow2MinusOne(ConstantInt*);
 
 bool runOnBasicBlock(BasicBlock &B) {
+  outs()<<"Optimizing Priority:\n"
+          "\t1. ALGEBRAIC IDENTITY\n"
+          "\t2. ADVANCED STRENGTH REDUCTION\n"
+          "\t3. MULTI-INSTRUCTION OPTIMIZATION\n"
+          "\t4. BASIC STRENGTH REDUCTION\n\n";
     for (auto &i: B){
-      // Algebraic Identity takes priority over Basic Strength Reduction
-      AlgebraicId(i) || AdvancedSR(i) || BasicSR(i);
+      outs()<<i<<"\n";
+      AlgebraicId(i) || AdvancedSR(i) || MultiInstOpt(i) || BasicSR(i);
 
-      // Advanced Strength Reduction
-      
-      
-      // Multi-Instruction Optimization
-      MultiInstOpt(i);
     }
     return true;
   }
@@ -50,8 +50,9 @@ bool runOnFunction(Function &F) {
 
 bool BasicSR(Instruction &i){
   BinaryOperator *mul = dyn_cast<BinaryOperator>(&i);
-  if (not mul or mul->getOpcode() != BinaryOperator::Mul)
+  if (not mul or mul->getOpcode() != BinaryOperator::Mul){
     return false;
+  }
   
   ConstantInt *C;
   Value *Factor;
@@ -62,17 +63,16 @@ bool BasicSR(Instruction &i){
   if (not C or not C->getValue().isPowerOf2()){
     C = dyn_cast<ConstantInt>(mul->getOperand(1));
     if (not C or not C->getValue().isPowerOf2()){
-      outs()<< *mul << ": no operand is a constant integer or power of 2\n";
       return false;
     }
 
     Factor = mul->getOperand(0);
   }
   
-  outs()<<"Converting "<< *mul << " to left shift\n";
   Constant *shiftConst = ConstantInt::get(C->getType(), C->getValue().exactLogBase2());
   Instruction *new_shift = BinaryOperator::Create(BinaryOperator::Shl, Factor, shiftConst);
   new_shift->insertAfter(mul);
+  outs()<<"\t✓ BasicSR Executed\n";
   mul->replaceAllUsesWith(new_shift);
 
   return true;
@@ -80,8 +80,9 @@ bool BasicSR(Instruction &i){
 
 bool AlgebraicId(Instruction &i){
   auto opCode = i.getOpcode();
-  if (opCode != Instruction::Add and opCode != Instruction::Mul)
+  if (opCode != Instruction::Add and opCode != Instruction::Mul){
     return false;
+  }
   
   Value *Factor = i.getOperand(0);
   ConstantInt *C = dyn_cast<ConstantInt>(i.getOperand(1));
@@ -90,7 +91,6 @@ bool AlgebraicId(Instruction &i){
     if(not C or not C->getValue().isZero()){
       C = dyn_cast<ConstantInt>(i.getOperand(0));
       if(not C or not C->getValue().isZero()){
-        outs()<< i << ": no operand is 0\n";
         return false;
       }
       Factor = i.getOperand(1);
@@ -99,12 +99,12 @@ bool AlgebraicId(Instruction &i){
       if(not C or not C->getValue().isOne()){
       C = dyn_cast<ConstantInt>(i.getOperand(0));
       if(not C or not C->getValue().isOne()){
-        outs()<< i << ": no operand is 1\n";
         return false;
       }
       Factor = i.getOperand(1);
       }
     }
+  outs()<<"\t✓ AlgebraicId Executed\n";
   i.replaceAllUsesWith(Factor);
   return true;
 }
@@ -119,11 +119,11 @@ bool isPow2MinusOne(ConstantInt *C){
 
 
 bool AdvancedSR(Instruction &i){
-  
   auto opCode = i.getOpcode();
-  if (opCode != Instruction::Mul and opCode != Instruction::SDiv){
+  if (opCode != Instruction::Mul and opCode != Instruction::SDiv)
     return false;
-  }
+  
+
   Value *Factor = i.getOperand(0);
   ConstantInt *C = dyn_cast<ConstantInt>(i.getOperand(1));
 
@@ -131,17 +131,14 @@ bool AdvancedSR(Instruction &i){
     if(not C or not(C->getValue().isPowerOf2() or isCloseToPow2(C))){
         C = dyn_cast<ConstantInt>(i.getOperand(0));
         
-        if(not C or not(C->getValue().isPowerOf2() or isCloseToPow2(C))){
-          outs()<< i << ": no operand is a constant integer nor power of 2, or close to it\n";
+        if(not C or not(C->getValue().isPowerOf2() or isCloseToPow2(C)))
           return false;
-        }
         Factor = i.getOperand(1);
     }
   }else{
-    if(not C or not C->getValue().isPowerOf2()){
-      outs()<< i << ": no right operand is a constant integer nor power of 2\n";
+    if(not C or not C->getValue().isPowerOf2())
       return false;
-    }
+    
   }
   Instruction::BinaryOps shiftType = (opCode == Instruction::Mul) ? BinaryOperator::Shl : BinaryOperator::LShr;
   Instruction::BinaryOps opType = isPow2MinusOne(C) ? BinaryOperator::Add : BinaryOperator::Sub;
@@ -154,12 +151,14 @@ bool AdvancedSR(Instruction &i){
 
       new_shift->insertAfter(&i);
       new_adapt->insertAfter(new_shift);
+      outs()<<"\t✓ AdvancedSR Executed\n";
       i.replaceAllUsesWith(new_adapt);
   }
   else{
     Constant *shiftConst = ConstantInt::get(C->getType(), C->getValue().exactLogBase2());
     Instruction *new_shift = BinaryOperator::Create(shiftType, Factor, shiftConst);
     new_shift->insertAfter(&i);
+    outs()<<"\t✓ AdvancedSR Executed\n";
     i.replaceAllUsesWith(new_shift);
   }
 
@@ -167,6 +166,41 @@ bool AdvancedSR(Instruction &i){
 }
 
 bool MultiInstOpt(Instruction &i){
+  //verificare che sia add o sub
+  auto opCode = i.getOpcode();
+  if (opCode != Instruction::Add and opCode != Instruction::Sub)
+    return false;
+  
+  //verificare che uno dei due operandi sia una costante e salvarla in C
+  Value *Factor = i.getOperand(0);
+  ConstantInt *C = dyn_cast<ConstantInt>(i.getOperand(1));
+
+  if(not C){
+      C = dyn_cast<ConstantInt>(i.getOperand(0));
+      if(not C)
+        return false;
+
+      Factor = i.getOperand(1);
+  }
+  //scorrere gli utilizzi dell'istruzione
+  for (auto userIter = i.user_begin(); userIter != i.user_end(); ++userIter) {
+    Instruction *user = dyn_cast<Instruction>(*userIter);
+    //tra quelli trovati, verificare che siano add o sub, rispettivamente se l'istruzione iniziale è sub o add
+    Instruction::BinaryOps oppositeType = (opCode==Instruction::Add) ? BinaryOperator::Sub : BinaryOperator::Add;
+    if (user->getOpcode() != oppositeType) continue;
+
+    ConstantInt *COpp = dyn_cast<ConstantInt>(user->getOperand(1));
+    if(not COpp){
+        COpp = dyn_cast<ConstantInt>(user->getOperand(0));
+        if(not COpp)
+          return false;     
+    }
+    //se esiste una costante tra gli operandi, verificare che sia uguale a C
+    if(COpp->getValue()!=C->getValue()) continue;
+    
+    outs()<<"\t✓ MultiInstOpt Executed\n" ;
+    user->replaceAllUsesWith(Factor);
+  }
   return true;
 }
 
